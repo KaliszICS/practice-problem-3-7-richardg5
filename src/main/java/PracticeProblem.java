ac5c637e7af4cdee2d9eab45a31cd0b88f6c16cf
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-04-09 17:33:34 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,43 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	
+	public static int searchMazeMoves(String[][] arr) {
+		return dfs(arr, arr.length - 1, 0, 0);
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+	public static int dfs(String[][] arr, int row, int col, int steps) {
+		if (row < 0 || col < 0 || row >= arr.length || col >= arr[row].length || arr[row][col].equals("*")) {
+			return -1;
+		} else if (arr[row][col].equals("F")) {
+			return steps;
+		}
 
-	public static void q3() {
-		//Write question 3 code here
+		arr[row][col] = "*";
+		int up = dfs(arr, row - 1, col, steps + 1);
+		int right = dfs(arr, row, col + 1, steps + 1);
+		int left = dfs(arr, row, col - 1, steps + 1);
+		int down = dfs(arr, row + 1, col, steps + 1);
+		arr[row][col] = "";
+		if (up == -1) {
+			up = arr.length * arr[row].length + 1;
+		}
+		if (right == -1) {
+			right = arr.length * arr[row].length + 1;
+		}
+		if (left == -1) {
+			left = arr.length * arr[row].length + 1;
+		}
+		if (down == -1) {
+			down = arr.length * arr[row].length + 1;
+		}
+		int min = Math.min(up, Math.min(right, Math.min(left, down)));
+		if (min == arr.length * arr[row].length + 1) {
+			min = -1;
+		}
+		return min;
 	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
